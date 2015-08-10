4529a4e89ed30013ee0b5802b38115ce3e9b6402
40thoughts <40thoughts@gmail.com>
2015-08-10 06:43:35 +0200

    Check if user is [root], and turned of the "wget" output

--- install.sh
+++ install.sh
@@ -3,13 +3,19 @@
 binFolder="/usr/bin"
 confFolder="/usr/share/diceword"
 
+if [ "$(id -u)" != "0" ]
+then
+  printf "\nThis script must be run as root\n\n"
+  exit 1
+fi
+
 if [ ! -d "$confFolder" ]
 then
   mkdir -p $confFolder
 fi
 
-wget https://raw.githubusercontent.com/40thoughts/bash-Diceword/master/res/diceware.json -O $confFolder/diceware.json
-wget https://raw.githubusercontent.com/40thoughts/bash-Diceword/master/diceword -O $binFolder/diceword
+wget -q https://raw.githubusercontent.com/40thoughts/bash-Diceword/master/res/diceware.json -O $confFolder/diceware.json
+wget -q https://raw.githubusercontent.com/40thoughts/bash-Diceword/master/diceword -O $binFolder/diceword
 chmod +x $binFolder/diceword
 
 printf "\nDiceword has been succesfully installed\n"
